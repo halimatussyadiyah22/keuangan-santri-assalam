ad15534521fa72040ced37ecc52ba06ba8c0afeb
LAB 2B-02 <halimahsyadiyah2202@gmail.com>
2023-08-12 02:22:13 -0700

    projek

---
+++ src/app/component/menu-bar/menu-bar.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+import {MenuItem} from "primeng/api";
+
+@Component({
+  selector: 'app-menu-bar',
+  templateUrl: './menu-bar.component.html',
+  styleUrls: ['./menu-bar.component.scss']
+})
+export class MenuBarComponent {
+
+}
---
+++ src/app/component/menu-item/menu-item.component.ts
@@ -0,0 +1,9 @@
+import {Component, Input} from '@angular/core';
+import {MenuItem} from "primeng/api";
+
+@Component({
+  selector: 'app-menu-item',
+  templateUrl: './menu-item.component.html',
+  styleUrls: ['./menu-item.component.scss']
+})
+export class MenuItemComponent {}
